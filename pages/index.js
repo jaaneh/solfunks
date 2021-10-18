310073c6535e165913cedd96c29a40ca21ad4297
Jan Henning <triton539@outlook.com>
2021-10-18 21:16:37 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>solfunks.com</h1>
+    </div>
+  );
+};
+
+export default Home;
